52cd23b75482af6067a0fee47a45f750906ffa26
mirko-27-93 <mirko.vidakovic.2020.002@gmail.com>
2020-08-26 19:07:08 +0200

    001YIKorisniciE01P002_BazaPodataka
    Registracija korisnika.
    Korisnicke sesije.
    Korisnicka baza.
    
    Baza podataka.

---
+++ COMMANDS/data.delete.sql
@@ -0,0 +1,6 @@
+DELETE FROM yi.userinfo; 
+DELETE FROM ieee.userinfo; 
+DELETE FROM yatospace.userinfo; 
+DELETE FROM yimp.userinfo;
+DELETE FROM yimk.userinfo; 
+DELETE FROM ip.userinfo; 
---
+++ DB-MYSQL-DML-QUERY/TABLE-1/yi.sql
@@ -0,0 +1,1 @@
+SELECT * FROM yi.userinfo; 
